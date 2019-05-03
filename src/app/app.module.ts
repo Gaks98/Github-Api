cafd98cf29a5e15b74383d50b2b0788da868a021
Gaks98 <yvonnegax98@gmail.com>
2019-05-03 17:01:31 +0300

    imported the HttpModule in the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 //import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +17,7 @@ import { ProfileService } from './services/profile.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-   // HttpModule,
+    HttpModule,
    // FormsModule
   ],
   providers: [ProfileService],
